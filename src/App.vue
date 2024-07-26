deca3bf3264ba6a794d171548b995b015c8f644f
luiz hakan <luizhakan2@protonmail.com>
2024-07-25 22:35:47 -0300

    correções solicitadas no design

--- src/App.vue
+++ src/App.vue
@@ -72,57 +72,88 @@ const togglePasswordVisibility = () => {
 </script>
 
 <template>
-  <body class="flex justify-center items-center min-h-screen">
+  <body class="flex justify-center items-center min-h-screen max-[767px]:mx-[2rem]">
     <div
-      class="container bg-[#212226] w-full max-w-lg flex flex-col md:flex-row px-8 md:px-16 justify-center items-center rounded py-8"
+      class="container w-[50rem] bg-[#212226] flex flex-col md:flex-row justify-center items-center rounded py-20 px-4 md:py-20 md:px-8"
     >
-      <div class="logos flex flex-row md:flex-col items-center md:items-start">
+      <div class="logos flex flex-col items-center">
         <img
-          src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/facebook/facebook-original.svg"
-          class="w-[4rem] h-[4rem] my-2 mx-2 cursor-pointer"
-        />
-        <img
-          src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/google/google-original.svg"
-          class="w-[4rem] h-[4rem] my-2 mx-2 cursor-pointer"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Facebook_logo_%28square%29.png/800px-Facebook_logo_%28square%29.png"
+          class="w-[20rem] h-[17rem] cursor-pointer"
         />
       </div>
 
-      <div class="or my-4 md:my-0 mx-0 md:mx-4">
-        <span class="rounded-full bg-black p-4 text-white font-bold block md:inline-block">or</span>
+      <div class="or my-4 md:my-0 mx-0 md:mx-[-.7rem] self-start z-50 max-[767px]:self-center">
+        <span class="rounded-[100%] bg-[#212226] p-3 text-white font-bold block">or</span>
       </div>
 
-      <div class="form bg-[#17232c] p-8 rounded w-full mb-4">
-        <h1 class="text-white font-bold text-center md:text-left">Log in with awesome new thing</h1>
-        <form
-          @submit="handleSubmit"
-          class="flex flex-col items-center md:flex-row md:relative w-full"
-        >
-          <div class="flex flex-col w-full">
+      <div class="form bg-[#535556] content-center p-4 rounded w-full md:w-[30rem] md:h-[17rem]">
+        <h1 class="text-white font-bold text-[1.3rem] text-center whitespace-nowrap mb-4">
+          Log in with awesome new thing
+        </h1>
+        <form @submit="handleSubmit" class="flex flex-col items-center md:relative w-full">
+          <div class="flex flex-col w-full relative">
             <input
               type="text"
               v-model="usernameOrEmail"
               @input="validateField('usernameOrEmail')"
               @blur="validateField('usernameOrEmail')"
-              class="my-2 p-2 w-full rounded bg-[#111a21] text-white"
+              class="my-2 mb-6 p-2 w-full rounded bg-[#323232] text-white pr-12"
               placeholder="username or email"
             />
-            <span class="text-red-500" v-if="errors.usernameOrEmail">{{
+            <span class="text-red-500 error-message mt-[-1rem]" v-if="errors.usernameOrEmail">{{
               errors.usernameOrEmail
             }}</span>
-
+            <button
+              class="login-button absolute top-[30%] right-[-2.5rem] transform -translate-y-1/2 -translate-x-1/4 z-50"
+            >
+              <svg
+                v-if="!loading"
+                xmlns="http://www.w3.org/2000/svg"
+                class="icon"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M9 5l7 7-7 7"
+                />
+              </svg>
+              <svg
+                v-else
+                class="animate-spin h-6 w-6 text-white"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <circle
+                  class="opacity-25"
+                  cx="12"
+                  cy="12"
+                  r="10"
+                  stroke="currentColor"
+                  stroke-width="4"
+                ></circle>
+                <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8v8H4z"></path>
+              </svg>
+            </button>
             <div class="relative">
               <input
                 :type="showPassword ? 'text' : 'password'"
                 v-model="password"
                 @input="validateField('password')"
                 @blur="validateField('password')"
-                class="p-2 w-full rounded bg-[#111a21] text-white"
+                class="p-2 w-full rounded bg-[#323232] text-white pr-12"
                 placeholder="password"
               />
               <button
                 type="button"
                 @click="togglePasswordVisibility"
-                class="absolute right-2 top-1/2 transform -translate-y-1/2 text-white"
+                class="absolute right-8 top-1/2 transform -translate-y-1/2 text-white"
               >
                 <svg
                   v-if="!showPassword"
@@ -161,48 +192,12 @@ const togglePasswordVisibility = () => {
                 </svg>
               </button>
             </div>
-            <span class="text-red-500" v-if="errors.password">{{ errors.password }}</span>
+            <span class="text-red-500 error-message" v-if="errors.password">{{
+              errors.password
+            }}</span>
           </div>
-
-          <button
-            class="login-button mt-4 md:mt-0 md:absolute md:right-[-4rem] md:top-1/2 md:transform md:-translate-y-1/2"
-          >
-            <svg
-              v-if="!loading"
-              xmlns="http://www.w3.org/2000/svg"
-              class="icon"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M9 5l7 7-7 7"
-              />
-            </svg>
-            <svg
-              v-else
-              class="animate-spin h-6 w-6 text-white"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <circle
-                class="opacity-25"
-                cx="12"
-                cy="12"
-                r="10"
-                stroke="currentColor"
-                stroke-width="4"
-              ></circle>
-              <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8v8H4z"></path>
-            </svg>
-          </button>
         </form>
-        <a href="#" class="text-[#bec2ca] font-bold block text-center md:text-left mt-2"
+        <a href="#" id="forgot-password" class="font-medium block text-center md:text-left mt-6"
           >Forgot your password?</a
         >
       </div>
@@ -211,20 +206,25 @@ const togglePasswordVisibility = () => {
 </template>
 
 <style scoped>
+#forgot-password {
+  color: rgb(161, 161, 161);
+}
+
 .login-button {
   background-color: #f97316;
-  border: 2px solid black;
+  border: 5px solid #323232;
   border-radius: 50%;
-  padding: 1rem;
+  padding: 0.75rem;
   display: flex;
   align-items: center;
   justify-content: center;
   cursor: pointer;
+  transform: scale(1.5);
 }
 
 .icon {
-  height: 2rem;
-  width: 2rem;
+  height: 1.5rem;
+  width: 1.5rem;
   stroke: white;
 }
 
@@ -257,5 +257,9 @@ input:focus {
     -webkit-transform: rotate(360deg);
     transform: rotate(360deg);
   }
+}
+
+.error-message {
+  height: 1.5rem; /* Define a fixed height to avoid changing the layout */
 }
 </style>
